c4f511292d12097549fb36a40a66289c195ed4b0
miclon <jcnd@163.com>
2023-08-02 10:03:25 +0800

    fix: webdriver fake

---
+++ playwright_stealth/js/navigator.webdriver.js
@@ -0,0 +1,13 @@
+Object.defineProperty(Object.getPrototypeOf(navigator), 'webdriver', {
+    set: undefined,
+    enumerable: true,
+    configurable: true,
+    get: new Proxy(
+        Object.getOwnPropertyDescriptor(Object.getPrototypeOf(navigator), 'webdriver').get,
+        { apply: (target, thisArg, args) => {
+            // emulate getter call validation
+            Reflect.apply(target, thisArg, args);
+            return false;
+        }}
+    )
+});
