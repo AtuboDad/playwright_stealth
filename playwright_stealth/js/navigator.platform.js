1df83f9a42ca54d3196e4606913349a58111001d
Pataposha <pataposha.dev@gmail.com>
2023-09-29 12:46:13 +0300

    fix typo: `navigator_plaftorm` -> `navigator_platform`

--- playwright_stealth/js/navigator.platform.js
+++ playwright_stealth/js/navigator.platform.js
@@ -1,5 +1,5 @@
 if (opts.navigator_platform) {
     Object.defineProperty(Object.getPrototypeOf(navigator), 'platform', {
-        get: () => opts.navigator_plaftorm,
+        get: () => opts.navigator_platform,
     })
 }
